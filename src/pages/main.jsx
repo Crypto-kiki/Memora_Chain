a32bb9a2e941fd726b4272583a52d09c6eeed88a
Crypto-kiki <ejo2123@naver.com>
2023-06-22 13:41:15 +0900

    dotenv

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,8 +1,3 @@
-import firebase from "firebase/app";
-import "firebase/database";
-import { useState } from "react";
-import database from "./firebase.js";
-
 const Main = () => {
   return <div></div>;
 };
